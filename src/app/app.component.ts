42fb427f463bde6687e5e0da2a94705c22c7eec8
Tristan Kernan <russon77@gmail.com>
2017-12-20 13:23:50 -0500

    feat: star wars icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,6 +16,34 @@ export class AppComponent implements OnInit {
     'Missing Content': 'is-primary'
   };
 
+  public swIcons = [
+    'admiral-ackbar.png',
+    'darth-maul.png',
+    'han-solo.png',
+    'obiwan-kenobi.png',
+    'stormtrooper.png',
+    'boba-fett.png',
+    'darth-vader.png',
+    'jabba-the-hutt.png',
+    'princess-leia.png',
+    'tusken-raider.png',
+    'c3p0.png',
+    'emperor-palpatine.png',
+    'jango-fett.png',
+    'qui-gon-jinn.png',
+    'yoda.png',
+    'chewbacca.png',
+    'ewok.png',
+    'jawa.png',
+    'lobot.png',
+    'r2d2.png',
+    'clone-trooper.png',
+    'greedo.png',
+    'lando-calrissian.png',
+    'luke-skywalker.png',
+    'red-five.png'
+  ];
+
   public categoriesFilter: string[] = [];
   public sortFilter = 'Votes';
 
